6c3882f97c8c2946c82a04c91653d27f92da6d47
Breno <onerbreno@outlook.com>
2023-02-27 09:30:15 -0300

    Capítulo 2 - repetições

--- cap2-decisoes/DecisaoComposta.py
+++ cap2-decisoes/DecisaoComposta.py
@@ -1,7 +1,8 @@
-nome=input("Digite o nome: ")
-idade=int(input("Digite a idade: "))
-doenca_infectocontagiosa=input("Suspeita de doença infecto-contagiosa?").upper()
-if idade>=65 and doenca_infectocontagiosa=="SIM":
+nome = input("Digite o nome: ")
+idade = int(input("Digite a idade: "))
+
+doenca_infectocontagiosa = input("Suspeita de doença infecto-contagiosa?").upper()
+if idade >= 65 and doenca_infectocontagiosa == "SIM":
     print("O paciente será direcionado para sala AMARELA - COM prioridade")
 elif idade < 65 and doenca_infectocontagiosa == "SIM":
     print("O paciente será direcionado para sala AMARELA - SEM prioridade")
--- cap2-repeticoes/DesafioDecisaoWhile.py
+++ cap2-repeticoes/DesafioDecisaoWhile.py
@@ -1,19 +1,19 @@
 resposta="SIM"
-while resposta=="SIM":
-    nivel=input("Digite o nível de acesso: ").upper()
-    if nivel=="ADM" or nivel=="USR":
-        genero=input("Digite o seu gênero: ").upper()
-        if nivel=="ADM":
-            if genero=="FEMININO":
+while resposta == "SIM":
+    nivel = input("Digite o nível de acesso: ").upper()
+    if nivel == "ADM" or nivel == "USR":
+        genero = input("Digite o seu gênero: ").upper()
+        if nivel == "ADM":
+            if genero == "FEMININO":
                 print("Olá administradora")
             else:
                 print("Olá administrador")
         else:
-            if genero=="FEMININO":
+            if genero == "FEMININO":
                 print("Olá usuária")
             else:
                 print("Olá usuario")
-    elif nivel=="GUEST":
+    elif nivel == "GUEST":
         print("Olá visitante")
     else:
         print("Olá desconhecido(a)")
